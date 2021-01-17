01ea528aa35bfd16532e76436047c875c5de1a67
Julian Yu <julian.yu19@imperial.ac.uk>
2021-01-17 17:24:33 +0000

    first commit

---
+++ tetris/combinations.py
@@ -0,0 +1,23 @@
+orders = [(1,2,3),(1,2,0),(1,0,2),(1,0,3),(0,2,3),(0,2,1),(0,1,2),(0,1,3)]
+
+itj1 = ['jit','tij','tji','ijt','jti','itj','itj1.PNG']
+itj2 = ['jit','ijt','itj2.PNG']
+ilt1 = ['til','itl','tli','itl1.PNG']
+ilt2 = ['lit','lti','itl2.PNG']
+tjs1 = ['tjs','tsj','tjs1.PNG']
+tjs2 = ['jst','tjs2.PNG']
+ilz1 = ['zil','izl','zli','ilz1.PNG']
+jsz1 = ['zjs','zsj','jsz1.PNG']
+jsz2 = ['jzs','jsz2.PNG']
+ito1 = ['ito','tio','toi','ito1.PNG']
+ioj1 = ['oij','ioj','oji','ioj1.PNG']
+ijs1 = ['isj','sij','sji','ijs1.PNG']
+its1 = ['its','tis','its1.PNG']
+tlj1 = ['tjl','tlj','tlj1.PNG']
+tsz1 = ['tsz','tsz1.PNG']
+tsz2 = ['tzs','tsz2.PNG']
+toj1 = ['ojt','toj1.PNG']
+
+combos = [itj1,itj2,ilt1,ilt2,tjs1,tjs2,ilz1,jsz1,jsz2,ito1,ioj1,ijs1,its1,tlj1,tsz1,tsz2,toj1]
+
+#nospin = [ioj1,itj1,itj2,ilt1,ilt2,ito1,tlj1,toj1]
---
+++ tetris/pc.py
@@ -0,0 +1,50 @@
+from PIL import Image, ImageOps
+import os
+from combinations import *
+
+def flip(seq):
+    seq = seq.replace('l','x')
+    seq = seq.replace('j','l')
+    seq = seq.replace('x','j')
+    seq = seq.replace('s','x')
+    seq = seq.replace('z','s')
+    seq = seq.replace('x','z')
+    return seq
+
+def solve(p,reverse=False):
+    """
+    p: a 4-letter string, the first is the hold piece, and the next three are the three pieces you have next.
+    reverse: False if the hook is on the right, True if the hook is on the left.
+    
+    Returns images of all possible perfect clear sequences. If reverse is True, the color of some of the blocks 
+    might be wrong.
+    """
+    flag = 0
+    
+    possible = set()
+    for a, b, c in orders:
+        possible.add(p[a]+p[b]+p[c])
+    
+    if reverse == False:
+        for seq in possible:
+            for c in combos:
+                if seq in c:
+                    flag = 1
+                    print(seq)
+                    image = Image.open(f'pictures/{c[-1]}')
+                    image = image.resize((394,214))
+                    display(image)
+    else:
+        for seq in possible:
+            for c in combos:
+                if flip(seq) in c:
+                    flag = 1
+                    print(seq)
+                    image = Image.open(f'pictures/{c[-1]}')
+                    image = image.resize((394,214))
+                    image = ImageOps.mirror(image)
+                    display(image)
+        
+    if flag == 0:
+        print('YOU ARE FUCKED')
+    
